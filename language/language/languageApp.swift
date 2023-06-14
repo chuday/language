15b49e2dcbc0f612f4ddc695b5a87cac247a9f22
chuday <mikhail_chudaew505@mail.ru>
2023-06-14 08:55:59 +0500

    Create app

---
+++ language/language/languageApp.swift
@@ -0,0 +1,17 @@
+//
+//  languageApp.swift
+//  language
+//
+//  Created by Mikhail Chudaev on 14.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct languageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
